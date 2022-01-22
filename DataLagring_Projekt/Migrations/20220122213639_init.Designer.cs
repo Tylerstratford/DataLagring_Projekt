4113390e9b4b30e43f6136b496d1d55960de0d8b
Tylerstratford <tylerstratford@hotmail.com>
2022-01-22 23:04:09 +0100

    .

--- DataLagring_Projekt/Migrations/20220122213639_init.Designer.cs
+++ DataLagring_Projekt/Migrations/20220122213639_init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataLagring_Projekt.Migrations
 {
     [DbContext(typeof(SqlContext))]
-    [Migration("20220119194513_init")]
+    [Migration("20220122213639_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
